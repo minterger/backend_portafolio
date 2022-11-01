600f126f60cbfe2bee42dbd8eaa03c5097cc2175
Leonel Menci <minterger@gmail.com>
2022-11-01 13:16:09 -0300

    Update ProjectController.java

--- src/main/java/com/argentina_programa/backend_portafolio/controllers/ProjectController.java
+++ src/main/java/com/argentina_programa/backend_portafolio/controllers/ProjectController.java
@@ -1,14 +1,20 @@
 package com.argentina_programa.backend_portafolio.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.argentina_programa.backend_portafolio.Exceptions.ResourceNotFoundException;
 import com.argentina_programa.backend_portafolio.models.Project;
 import com.argentina_programa.backend_portafolio.repositories.ProjectRepositorio;
 
@@ -27,5 +33,43 @@ public class ProjectController {
     @PostMapping
     public Project newProject(@RequestBody Project project) {
         return repositorio.save(project);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> getProject(@RequestBody Project projectBody, Long id) {
+        Project project = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El Proyecto no fue encontrado"));
+        return ResponseEntity.ok(project);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> ediProject(@RequestBody Project projectBody, Long id) {
+        Project project = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El Proyecto no fue encontrado"));
+
+        project.setName(projectBody.getName());
+        project.setDescription(projectBody.getDescription());
+        project.setImgUrl(projectBody.getImgUrl());
+        project.setLinkGithub(projectBody.getLinkGithub());
+        project.setLinkProject(projectBody.getLinkProject());
+        project.setTecnologies(projectBody.getTecnologies());
+
+        Project responseProject = repositorio.save(project);
+
+        return ResponseEntity.ok(responseProject);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Map<String, Boolean>> deleteProject(Long id) {
+        Project project = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El Proyecto no fue encontrado"));
+
+        repositorio.delete(project);
+
+        Map<String, Boolean> response = new HashMap<>();
+
+        response.put("Eliminar", true);
+        return ResponseEntity.ok(response);
+
     }
 }
