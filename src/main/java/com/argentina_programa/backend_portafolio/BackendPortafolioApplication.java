aa9bb09f59ecb41059b28de438e01d21583b3515
Leonel Menci <minterger@gmail.com>
2022-11-01 12:05:01 -0300

    try add cors

--- src/main/java/com/argentina_programa/backend_portafolio/BackendPortafolioApplication.java
+++ src/main/java/com/argentina_programa/backend_portafolio/BackendPortafolioApplication.java
@@ -2,12 +2,27 @@ package com.argentina_programa.backend_portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BackendPortafolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendPortafolioApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("/**").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
