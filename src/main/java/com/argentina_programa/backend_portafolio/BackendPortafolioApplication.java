f4b69ad00f0d190f99f99fa98055757d3b70fdb8
Leonel Menci <minterger@gmail.com>
2022-10-31 21:24:54 -0300

    update

--- src/main/java/com/argentina_programa/backend_portafolio/BackendPortafolioApplication.java
+++ src/main/java/com/argentina_programa/backend_portafolio/BackendPortafolioApplication.java
@@ -2,12 +2,29 @@ package com.argentina_programa.backend_portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class BackendPortafolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendPortafolioApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/producto/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.maxAge(3600);
+			}
+
+		};
 	}
 
 }
--- src/main/java/com/argentina_programa/backend_portafolio/controllers/ProjectController.java
+++ src/main/java/com/argentina_programa/backend_portafolio/controllers/ProjectController.java
@@ -3,7 +3,6 @@ package com.argentina_programa.backend_portafolio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,22 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.argentina_programa.backend_portafolio.models.Project;
 import com.argentina_programa.backend_portafolio.repositories.ProjectRepositorio;
 
-
 @RestController
 @RequestMapping("/project")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class ProjectController {
 
     @Autowired
     private ProjectRepositorio repositorio;
 
     @GetMapping
-    public List<Project> listProjects () {
+    public List<Project> listProjects() {
         return repositorio.findAll();
     }
 
     @PostMapping
-    public Project newProject (@RequestBody Project project) {
+    public Project newProject(@RequestBody Project project) {
         return repositorio.save(project);
     }
 }
